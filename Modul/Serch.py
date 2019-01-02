c2f1b4e61f443b9e5ada3a6c3958b775f5daeed6
dezmond22 <dezmond22@yandex.ru>
2019-01-02 21:35:28 +0300

    1

--- Modul/Serch.py
+++ Modul/Serch.py
@@ -1,43 +1,46 @@
-import vk_api
+import vk
 from my_data import MyVKData
 from config import whiteWordPart
+from config import blackWordPart
 import time
-def main():
-    vk_session = vk_api.VkApi(MyVKData.LOGIN, MyVKData.GET_PASSWORD)
-    try:
-        vk_session.auth()
-        print('Ok')
-    except vk_api.AuthError as errror_msg:
-        print(errror_msg)
-        return
+
+v=5.92
+session = vk.AuthSession(app_id=MyVKData.MY_PRIL_ID, user_login=MyVKData.LOGIN, user_password=MyVKData.GET_PASSWORD, scope='wall, fields, messages')
+vkapi = vk.API(session)
+api = vk.API(session, v=v)
+count = 200
+posts = []
+id_posts_data =[]
+stop = False
 
 
-    with vk_api.VkRequestsPool(vk_session) as pool:
-        posts = []
+while stop == False:
+    newsfeed = vkapi.newsfeed.search(q='конкурс репост подарки санкт-петербург', count=count, filters='post ', v=v)
 
-
-        #for word in whiteWordPart:
-            #time.sleep(1 / 3)
-        finded_groups = pool.method('groups.search', {
-            #'q': word
-            'q':'лайк'
-        })
-
-        for post in newsfeed.items:
-            id_posts = post['id_posts']
-            group_id = post['group_id']
+    for post in newsfeed['items']:
+            id_posts = post['from_id']
+            group_id = post['id']
+            text = post['text']
             mass = {}
             mass['id_posts'] = id_posts
             mass['group_id'] = group_id
-            posts.append(mass)
+            mass['text'] = text
+            i = 0
+            i_max=len(whiteWordPart)
+            while i < i_max and  whiteWordPart[i] in text:
+                i1=0
+                i1_max = len(blackWordPart)
+                while i1 < i1_max  and blackWordPart[i1] not in text:
+                    if id_posts not in id_posts_data:
+                        id_posts_data.append(id_posts)
+                        posts.append(mass)
+                    i1=i1+1
+                i=i+1
 
+    print(posts)
 
-for post in posts:
-    vkapi.wall.repost(post['id_posts'])
+    # for post in posts:
+    #     vkapi.wall.repost(post['id_posts'])
 
+    time.sleep(60)
 
-    print(groups)
-
-
-if __name__ == '__main__':
-    main()
